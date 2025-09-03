c8325d545a14e1210c81ad3c46f883617c4cd057
luispiamba97 <fernandopiamba97@gmail.com>
2025-09-02 23:21:54 -0500

    Se arregla print

--- Parcial/src/App.java
+++ Parcial/src/App.java
@@ -7,11 +7,11 @@ public class App {
         
        //Creamos libro 1
 
-       Libro libro1 = new Libro();
+       Libro libro1 = new Libro("Caleñisimo","Luis Fernando",15,7);
 
        //Creamos el libro 2
 
-       Libro libro2 = new Libro("Escalofrios","Robert .L",15,7);
+       Libro libro2 = new Libro();
     
        // datos para libro2
 
@@ -23,49 +23,56 @@ public class App {
        libro2.setNumeroEjemplares(scanner.nextInt());
        System.out.print("Introduce el número de ejemplares prestados del libro : ");
        libro2.setNumeroEjemplaresPrestados(scanner.nextInt());
-       System.out.println("--------------------------------");
+       System.out.println("\n--------------------------------\n");
 
-       System.out.println("\nDatos del Libro :");
+       System.out.println("\nDatos del Libro : \n");
        System.out.println(libro2.toString());
 
        System.out.println("---------------------------------");
 
        // Probar los métodos de prestamo y devolucion
-       System.out.println("Probando métodos de préstamo y devolución");
+       System.out.println("\nProbando métodos de préstamo y devolución\n");
 
        // Prueba con objeto libro1
-       System.out.println("Pruebas con Libro 1:");
+       System.out.println("Pruebas con Libro 1: \n");
        System.out.println("Intentando prestar un ejemplar de Libro 1...");
        if (libro1.prestamo()) {
-           System.out.println("Préstamo exitoso... \n Estado actual: " + libro1.toString());
+           System.out.println("\nPréstamo exitoso...\n");
+           System.out.println("\nEstado actual: " + libro1.toString());
        } else {
-           System.out.println("Préstamo fallido. No hay ejemplares disponibles.");
+           System.out.println("\nPréstamo fallido... No hay ejemplares disponibles.");
        }
 
-       System.out.println("Intentando devolver un ejemplar de Libro 1...");
+       System.out.println("\nIntentando devolver un ejemplar de Libro 1...");
        if (libro1.devolucion()) {
            System.out.println("Devolución exitosa... Estado actual: " + libro1.toString());
        } else {
            System.out.println("Devolución fallida!!! \n No hay ejemplares prestados.");
        }
 
-       System.out.println("---");
+       System.out.println("---------------------------\n");
 
        // Prueba con objeto libro2
        System.out.println("Pruebas con Libro 2:");
        System.out.println("Intentando prestar un ejemplar de Libro 2...");
        if (libro2.prestamo()) {
-           System.out.println("Préstamo exitoso. Estado actual: " + libro2.toString());
+           System.out.println("Préstamo exitoso ...!\n");
+           System.out.println("Estado actual: " + libro2.toString());
        } else {
-           System.out.println("Préstamo fallido. No hay ejemplares disponibles.");
+           System.out.println("\nPréstamo fallido. No hay ejemplares disponibles.");
        }
 
-       System.out.println("Intentando devolver un ejemplar de Libro 2...");
+       System.out.println("\nPrueba metodo devolver en objeto Libro 2...");
        if (libro2.devolucion()) {
-           System.out.println("Devolución exitosa. Estado actual: " + libro2.toString());
+           System.out.println("\nDevolución exitosa");
+           System.out.println("\nEstado actual: " + libro2.toString());
        } else {
-           System.out.println("Devolución fallida. No hay ejemplares prestados.");
+           System.out.println("\nDevolución fallida... No hay ejemplares prestados.\n");
        }
+
+       System.out.println("\n********************");
+       System.out.println("         F I N");
+       System.out.println("********************\n");
 
        scanner.close();
    }
