af5b1181d5ff7de829220572e78b5717173372d9
luispiamba97 <fernandopiamba97@gmail.com>
2025-09-02 19:35:24 -0500

    se agregan atributos a la clase libro desde rama LuisFPiamba

--- Parcial/src/Libro.java
+++ Parcial/src/Libro.java
@@ -2,5 +2,5 @@ public class Libro {
     private String titulo;
     private String autor;
     private int numeroEjemplares;
-    private int numEjemplaresPrestados;
+    private int numeroEjemplaresPrestados;
 }
