8a46afd762108c981da9cababc3876cbbc034fee
Djulian858 <velasquezjulian03@gmail.com>
2025-09-02 19:53:34 -0500

    se hizo el metodo constructor y se agrego el set y el get de titulo y autor

--- Parcial/src/Libro.java
+++ Parcial/src/Libro.java
@@ -3,4 +3,37 @@ public class Libro {
     private String autor;
     private int numeroEjemplares;
     private int numeroEjemplaresPrestados;
+    
+    //constructor vacio
+
+    public Libro(){}
+
+    public Libro(String titulo, String autor, int numeroEjemplares, int numeroEjemplaresPrestados)
+    {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroEjemplares = numeroEjemplares;
+        this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
+    }
+
+    public String getTitulo()
+    {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo)
+    {
+        this.titulo = titulo;
+    }
+
+    public String getAutor()
+    {
+        return autor;
+    }
+
+    public void setAutor(String autor)
+    {
+        this.autor = autor;
+    }
+
 }
