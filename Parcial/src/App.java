d55ccea907634f67c7d7e8c6c4eefd313b31c5d4
Djulian858 <velasquezjulian03@gmail.com>
2025-09-02 20:38:51 -0500

    Se crean los aobjetos libro 1 y libro 2

--- Parcial/src/App.java
+++ Parcial/src/App.java
@@ -1,5 +1,15 @@
+import javax.print.Doc;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Prueba");
+        
+       //Creamos libro 1
+
+       Libro libro1 = new Libro();
+
+       //Creamos el libro 2
+
+       Libro libro2 = new Libro("Escalofrios","Robert .L",15,7);
+
+
     }
 }
